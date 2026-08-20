08ec89af7f78eb33e12807fafe9d89a311c3d8b4
FiveMarks <mark1234zxcv@163.com>
2026-08-20 17:04:17 +0800

    test_map

--- card-game/scripts/map/MapData.gd
+++ card-game/scripts/map/MapData.gd
@@ -1,23 +1,40 @@
 class_name MapData;
 extends RefCounted;#用完就卸载，挂这个
 
-enum NodeType {
-	BATTLE,
-	ELITE,#精英
-	BOSS,
-	SHOP,
-	CAMPFIRE,
-	EVENT,
-	#
-};
-
+enum NodeType {BATTLE,ELITE,BOSS,SHOP,CAMPFIRE,EVENT,};
+#elite 精英
+var nodes: Dictionary = {}
 class MapNode:
 	var id: String;
 	var node_type: NodeType;
 	var position: Vector2;
-	var connection: Array[String] = [];
+	var edges: Array[String] = [];
 	var unlocked: bool = 0;
 	var completed: bool = 0;
 	
 	func _init(p_id: String, p_type: NodeType, p_pos: Vector2):
 		id = p_id;node_type = p_type;position = p_pos;
+
+func generate_test_map():
+	nodes.clear()
+	
+	# 第1层：起点
+	var start = MapNode.new("start", NodeType.BATTLE, Vector2(200,400));
+	start.unlocked = 1;
+	start.completed = 1;
+	nodes["start"] = start;
+	#t2
+	var node2 = MapNode.new("node2",NodeType.BATTLE,Vector2(400,400));
+	nodes["node2"] = node2;
+	nodes["start"].connections.append("node2");
+	#t3
+	var node3 = MapNode.new("node3",NodeType.BATTLE,Vector2(600,400));
+	nodes["node3"] = node3;
+	nodes["node2"].connections.append("node3");
+	#t4
+	var node4 = MapNode.new("node4",NodeType.BOSS,Vector2(800,400));
+	nodes["node4"] = node4;
+	nodes["node3"].connections.append("node4");
+	
+func get_node (id: String) -> MapNode:
+	return nodes.get(id, null);
