1fbe635f1f851957d6325e71ad16e4454db76805
FiveMarks <mark1234zxcv@163.com>
2026-08-27 20:59:22 +0800

    重组 Handcontainer.gd，独立CardEffectSystem，处理卡牌逻辑效果

---
+++ card-game/scripts/battle/CardEffectSystem.gd
@@ -0,0 +1,12 @@
+extends RefCounted;
+class_name  CardEffectSystem;
+static func execute_card_effect (card_data: CardData,target: Enemy,battle_manager: BattleManager):
+	# 伤害
+	if card_data.damage>0 && target:
+		target.take_damage(card_data.damage);
+		#print("%s 对 %s 造成 %d 点伤害" % [card_data.card_name, target.data.name, card_data.damage])
+	# 格挡
+	if card_data.block > 0:
+		battle_manager.player_block+=card_data.block;
+		battle_manager._update_player_ui();
+		#print("获得 %d 点格挡" % card_data.block)
